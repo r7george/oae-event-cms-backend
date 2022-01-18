5c3f332950443f32fe3b49d05ab1627a91636448
Reuben George <reuben_george1998@outlook.com>
2022-01-18 16:13:20 +0000

    exception handling for the different mapping methods done

--- src/main/java/com/example/oaeeventcms/OaeEventCmsController.java
+++ src/main/java/com/example/oaeeventcms/OaeEventCmsController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,32 +16,52 @@ public class OaeEventCmsController {
 
     @GetMapping("/events")
     public ResponseEntity<List<Event>> getEvents() {
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ArrayList<>());
+        }
     }
 
     @PostMapping("/events/add")
     public ResponseEntity<String> createEvent(@RequestBody Event event) {
-        repository.save(event);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Event added: " + event.getName());
+        try {
+            repository.save(event);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Event added: " + event.getName());
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error while trying to add new event");
+        }
     }
 
-    //Put the id within the body for the update to work
+    // Put the id within the body for the update to work
     @PutMapping("/events/{id}")
     public ResponseEntity<String> updateEventById(@RequestBody Event event, @PathVariable String id) {
-             repository.save(event);
-        return ResponseEntity.status(HttpStatus.OK).body("Event has been updated");
+        try {
+            repository.save(event);
+            return ResponseEntity.status(HttpStatus.OK).body("Event has been updated");
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error while trying to update event with id " + event.getId());
+        }
     }
 
     @GetMapping("events/{id}")
     public ResponseEntity<Event> getEventById(@PathVariable String id) {
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findEventByid(Integer.parseInt(id)));
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(repository.findEventByid(Integer.parseInt(id)));
+        } catch(Exception e) {
+            return ResponseEntity.status((HttpStatus.BAD_REQUEST)).body(null);
+        }
     }
 
     @DeleteMapping("events/{id}")
     @Transactional
     public ResponseEntity<String> deleteEventById(@PathVariable String id) {
-        repository.deleteEventByid(Integer.parseInt(id));
-        return ResponseEntity.status(HttpStatus.OK).body("Event with ID " + id + " has been deleted.");
+        try {
+            repository.deleteEventByid(Integer.parseInt(id));
+            return ResponseEntity.status(HttpStatus.OK).body("Event with ID " + id + " has been deleted.");
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error while trying to delete event with id " + repository.getById(id).getId());
+        }
     }
 
 }
